94f596a582ff0ceda71349b80a46a1403a5a3479
Enes-C <115008753+Enes-C@users.noreply.github.com>
2022-10-04 16:57:50 +0100

    Update Publication.h

--- Library/Library/Publication.h
+++ Library/Library/Publication.h
@@ -7,4 +7,32 @@ using namespace std;
 class Publication
 {
 
+<<<<<<< Updated upstream
+=======
+private:
+	string name;
+	int refNo;
+	bool isOnLoan;
+
+
+public :
+	void borrowPublication();
+	void returnPublication();
+
+
+	bool available;
+	string addPub;
+	void displayAllPub();
+	void displayDetails();
+	void displayPub();
+	void displayOnLoan();
+	void displayNumType(); 
+
+};
+
+void borrowPublication() {
+
+	if ((*this)isOnLoan)
+
+>>>>>>> Stashed changes
 }
